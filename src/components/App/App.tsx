1efb70f48eb8e5777558c9c8b0e2cc3d1b86d2f7
João Vitor <joaovitordesousapereira@gmail.com>
2023-02-09 15:17:38 -0300

    Create TestComponent and move App to components directory

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestComponent from '../TestComponent'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <TestComponent />
         </p>
         <a
           className="App-link"
---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './TestComponent.css'
+
+const TestComponent = () => (
+    <div className='TestComponent'>Test Componentinho</div>
+)
+    
+
+export default TestComponent;
