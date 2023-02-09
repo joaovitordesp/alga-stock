1e75ecf58a09bd3e5203410a65b1ab431fee2756
João Vitor <joaovitordesousapereira@gmail.com>
2023-02-09 17:15:59 -0300

    first useState

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './TestComponent.css'
 
-const TestComponent = () => (
-    <div className='TestComponent'>Test Componentinho</div>
-)
+// const TestComponent = () => (
+//     <div className='TestComponent'>Test Componentinho</div>
+// )
+
+function TestComponent (props: {name:string}){
+    const  [age, setAge] = useState(21)
+
+    return <div className='TestComponent'>
+       Olá, {props.name}, {age}
+
+       <button onClick={ () =>{
+        setAge(age + 1)
+       }}>
+        +
+       </button>
+
+       <button onClick={ () =>{
+        setAge(age - 1)
+       }}>
+        -
+       </button>
+    </div>
+
+}
     
 
 export default TestComponent;
