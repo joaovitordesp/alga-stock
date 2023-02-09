f22bef6695af9cc615187894c889ca999930a395
João Vitor <joaovitordesousapereira@gmail.com>
2023-02-09 16:53:10 -0300

    alter class component

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 class ClassComponent extends React.Component<{name:string}>{
+    constructor(props: any){
+        super(props);
+        console.log('constructor reached')
+    }
+    
     state ={
         name:'Mundo'
     }
 
+    componentDidMount(): void {
+        console.log('did mount reached')
+    }
+
+    componentDidUpdate(prevProps: Readonly<{ name: string; }>, prevState: Readonly<{}>, snapshot?: any): void {
+        console.log('did updated reached')
+    }
+
     render(){
+        console.log('render reached')
      return <div>
         <p>name: {this.state.name}</p>
         <button onClick={() =>{
